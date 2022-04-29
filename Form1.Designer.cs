95ad2c053ea049fab241647609dbab95de2b7c8b
Emirhan Doğan <76941464+EmirhanDgn@users.noreply.github.com>
2022-04-29 08:22:35 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Barcode_Uygulaması
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBarcode = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_olustur = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtBarcode
+            // 
+            this.txtBarcode.Location = new System.Drawing.Point(194, 27);
+            this.txtBarcode.Name = "txtBarcode";
+            this.txtBarcode.Size = new System.Drawing.Size(100, 20);
+            this.txtBarcode.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Barkoda Yazdırılacak Değer:";
+            // 
+            // btn_olustur
+            // 
+            this.btn_olustur.Location = new System.Drawing.Point(219, 70);
+            this.btn_olustur.Name = "btn_olustur";
+            this.btn_olustur.Size = new System.Drawing.Size(75, 23);
+            this.btn_olustur.TabIndex = 2;
+            this.btn_olustur.Text = "Barcode Oluştur";
+            this.btn_olustur.UseVisualStyleBackColor = true;
+            this.btn_olustur.Click += new System.EventHandler(this.btn_olustur_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(34, 142);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(260, 102);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 267);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_olustur);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBarcode);
+            this.Name = "Form1";
+            this.Text = "Barcode Oluşturucu";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBarcode;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_olustur;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Barcode_Uygulaması
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_olustur_Click(object sender, EventArgs e)
+        {
+            Zen.Barcode.Code128BarcodeDraw barcode = Zen.Barcode.BarcodeDrawFactory.Code128WithChecksum;
+            pictureBox1.Image = barcode.Draw(txtBarcode.Text, 50);
+
+
+        }
+    }
+}
